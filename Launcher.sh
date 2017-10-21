64943e9bc8851a2ab62016304169b6495ecbede8
Mintzmyer <samimintzmyer@gmail.com>
2017-10-21 13:39:28 -0700

    Starting the outline

---
+++ Launcher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage helper mesage
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./Launcher.sh has a number of commands
+Unfortunately, none of them are implemented yet :)
+
+Set a savefile location to store preset workspaces
+
+Set a new workspace preset
+
+Start a new workspace preset
+"
+fi
+
+# Set savefile location (2 params: set command and dir location)
+
+# Read from file functions (2 params: read command and file name)
+
+# Write to file functions (Overloaded)
+    # (1 param: write command launches list)
+
+
