64b35c6d98be44bacb37ff1afbc06323c0a0d162
Mintzmyer <samimintzmyer@gmail.com>
2017-10-26 21:51:22 -0700

    Added some scaffolding for the first functions

--- Launcher.sh
+++ Launcher.sh
@@ -14,10 +14,26 @@ Start a new workspace preset
 fi
 
 # Set savefile location (2 params: set command and dir location)
+if [ "$#" -eq 2 ] && [ $1 == "new" ]; then
+    echo "This command opens a new workspace configuration
+Unfortunately, this is not implemented yet
+"
+fi
 
 # Read from file functions (2 params: read command and file name)
 
+
 # Write to file functions (Overloaded)
     # (1 param: write command launches list)
+if [ $1 == "save" ]; then
+    echo "This command saves a workspace configuration for later use
+Unfortunately, this is not implemented yet
+"
+fi
 
-
+    # (2 param: write command and launch file)
+if [ "$#" -eq 2 ] && [ $1 == "save" ]; then
+    echo "This command takes a file as a second argument and saves it as a workspace for later use
+Unfortunately, this is not implemented yet
+"
+fi
