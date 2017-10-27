a2a7d5157c51b2fa98ce0740325beb0ff4d3331e
Mintzmyer <samimintzmyer@gmail.com>
2017-10-26 22:34:28 -0700

    Connected functions to if statements

--- Launcher.sh
+++ Launcher.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+OpenWorkspace(){
+    echo "This function will open a new workspace
+"
+}
+
+WriteWorkspace(){
+    echo "This function will allow users to create a workspace by entering programs, for later use
+"
+}
+
+SaveWorkspace(){
+    echo "This function will accept a file of programs and save it as a workspace for later use
+"
+}
+
+
+
+# Set savefile location (2 params: set command and dir location)
+if [ "$#" -eq 2 ] && [ $1 == "new" ]; then
+    SaveWorkspace
+
+# Write to file functions (Overloaded)
+    # (1 param: write command launches list)
+elif [ "$#" -eq 1 ] && [ $1 == "save" ]; then
+    WriteWorkspace
+
+    # (2 param: write command and launch file)
+elif [ "$#" -eq 2 ] && [ $1 == "save" ]; then
+    SaveWorkspace
+
 # Usage helper mesage
-if [ "$#" -ne 1 ]; then
+else
     echo "Usage: ./Launcher.sh has a number of commands
 Unfortunately, none of them are implemented yet :)
 
@@ -9,31 +39,6 @@ Set a savefile location to store preset workspaces
 
 Set a new workspace preset
 
-Start a new workspace preset
-"
-fi
-
-# Set savefile location (2 params: set command and dir location)
-if [ "$#" -eq 2 ] && [ $1 == "new" ]; then
-    echo "This command opens a new workspace configuration
-Unfortunately, this is not implemented yet
-"
-fi
-
-# Read from file functions (2 params: read command and file name)
-
-
-# Write to file functions (Overloaded)
-    # (1 param: write command launches list)
-if [ $1 == "save" ]; then
-    echo "This command saves a workspace configuration for later use
-Unfortunately, this is not implemented yet
-"
-fi
-
-    # (2 param: write command and launch file)
-if [ "$#" -eq 2 ] && [ $1 == "save" ]; then
-    echo "This command takes a file as a second argument and saves it as a workspace for later use
-Unfortunately, this is not implemented yet
+Open a new workspace preset
 "
 fi
